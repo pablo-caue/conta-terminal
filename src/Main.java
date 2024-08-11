cf2b6caadebe197a327d364ae4cdba2ce556b5cd
pablo-caue <pablocrs2004@gmail.com>
2024-08-11 14:36:18 -0300

    add: main.java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da conta");
+        int numAgencia = sc.nextInt();
+        System.out.println("Por favor, digite o numero da agencia");
+        String numConta = sc.next();
+        System.out.println("Por favor, digite seu nome");
+        String nomeCliente = sc.next();
+        System.out.println("Por ultimo, digite o saldo da conta");
+        float saldoConta = sc.nextFloat();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numAgencia + ", conta " + numConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+    }
+}
